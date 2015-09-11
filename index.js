087c01fe66348a21b77fa9abcfe2d3f4e4b153e3
Jooyong Sung <camon85@gmail.com>
2015-09-11 16:54:44 +0900

    배포테스트

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello camon!');
 });
 
+app.get('/test', function(request, response) {
+  response.send('test!');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
